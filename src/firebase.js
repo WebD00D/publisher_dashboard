ea79763f58c57a54fc161483aed08b04ad88fd27
Christian <rva.christian91@gmail.com>
2018-02-12 10:49:16 -0800

    adds dev configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 
 import firebase from 'firebase'
-var config = {
+
+var production = {
   apiKey: "AIzaSyCO99WM-4YXykyht60i7rjTOAQOdmy7o7M",
   authDomain: "the-library-1a359.firebaseapp.com",
   databaseURL: "https://the-library-1a359.firebaseio.com",
@@ -8,5 +9,15 @@ var config = {
   storageBucket: "the-library-1a359.appspot.com",
   messagingSenderId: "147199364885"
   };
-var fire = firebase.initializeApp(config);
+
+var dev = {
+    apiKey: "AIzaSyD2d0ky9h8vTRPijjfxtQs5iUOKsoMiYF0",
+    authDomain: "library-dev-1ccef.firebaseapp.com",
+    databaseURL: "https://library-dev-1ccef.firebaseio.com",
+    projectId: "library-dev-1ccef",
+    storageBucket: "",
+    messagingSenderId: "947409743016"
+}
+
+var fire = firebase.initializeApp(dev);
 export default fire;
